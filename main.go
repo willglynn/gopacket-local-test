6b424b1eb089757b722b55806e9a4ad52f6951ff
Will Glynn <will@willglynn.com>
2014-05-27 10:05:45 -0500

    Add a main.go for dependency resolution

---
+++ main.go
@@ -0,0 +1,12 @@
+package gopacket_local_test
+
+import (
+  _ "code.google.com/p/gopacket"
+  _ "code.google.com/p/gopacket/layers"
+  _ "code.google.com/p/gopacket/pcap"
+  "log"
+)
+
+func main() {
+  log.Fatal("run `go test` instead")
+}
