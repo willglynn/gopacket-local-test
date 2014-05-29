1f0ca9644c7a35c99b7e322f8c60b269f10ef99c
Will Glynn <will@willglynn.com>
2014-05-29 11:44:52 -0500

    `go fmt` because I need hooks

--- gopacket_local_test.go
+++ gopacket_local_test.go
@@ -11,98 +11,98 @@
 package gopacket_local
 
 import (
-  "code.google.com/p/gopacket"
-  _ "code.google.com/p/gopacket/layers"
-  "code.google.com/p/gopacket/pcap"
-  "flag"
-  "net"
-  "testing"
-  "time"
+	"code.google.com/p/gopacket"
+	_ "code.google.com/p/gopacket/layers"
+	"code.google.com/p/gopacket/pcap"
+	"flag"
+	"net"
+	"testing"
+	"time"
 )
 
 var iface = flag.String("interface", "first", "interface to use for capturing")
 
 func init() {
-  flag.Parse()
+	flag.Parse()
 }
 
 // Send a UDP datagram to 8.8.8.8 (Google public DNS) on the discard port
 func sendPacket(t *testing.T, done chan<- bool) {
-  target, err := net.ResolveUDPAddr("udp4", "8.8.8.8:9")
-  if err != nil {
-    t.Fatal(err)
-  }
+	target, err := net.ResolveUDPAddr("udp4", "8.8.8.8:9")
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  conn, err := net.DialUDP("udp4", nil, target)
-  if err != nil {
-    t.Fatal(err)
-  }
+	conn, err := net.DialUDP("udp4", nil, target)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  _, err = conn.Write([]byte("test packet, please ignore"))
-  if err != nil {
-    t.Fatal(err)
-  }
+	_, err = conn.Write([]byte("test packet, please ignore"))
+	if err != nil {
+		t.Fatal(err)
+	}
 
-  done <- true
+	done <- true
 }
 
 func TestCapture(t *testing.T) {
-  iface := *iface
-  if iface == "first" {
-    if ifs, err := pcap.FindAllDevs(); err != nil {
-      t.Fatal(err)
-    } else if len(ifs) > 0 {
-      iface = ifs[0].Name
-    } else {
-      t.Fatal("no interface specified and none found")
-    }
-  }
+	iface := *iface
+	if iface == "first" {
+		if ifs, err := pcap.FindAllDevs(); err != nil {
+			t.Fatal(err)
+		} else if len(ifs) > 0 {
+			iface = ifs[0].Name
+		} else {
+			t.Fatal("no interface specified and none found")
+		}
+	}
 
-  t.Logf("starting capture on interface %q", iface)
+	t.Logf("starting capture on interface %q", iface)
 
-  handle, err := pcap.OpenLive(iface, 1600, false, pcap.BlockForever)
-  if err != nil {
-    t.Fatal(err)
-  }
-  // XXX: this causes a hang and/or segfault
-  //defer handle.Close()
-	
+	handle, err := pcap.OpenLive(iface, 1600, false, pcap.BlockForever)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// XXX: this causes a hang and/or segfault
+	//defer handle.Close()
+
 	t.Logf("link type before setting filter: %d", handle.LinkType())
 
-  if err := handle.SetBPFFilter("host 8.8.8.8 && port 9"); err != nil {
-    t.Fatal(err)
-  }
+	if err := handle.SetBPFFilter("host 8.8.8.8 && port 9"); err != nil {
+		t.Fatal(err)
+	}
 
 	t.Logf("link type after setting filter: %d", handle.LinkType())
 
-  t.Logf("capturing on %q", iface)
+	t.Logf("capturing on %q", iface)
 
-  packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-  packets := packetSource.Packets()
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packets := packetSource.Packets()
 
-  // send a detectable test packet in another goroutine
-  packetSent := make(chan bool)
-  go sendPacket(t, packetSent)
+	// send a detectable test packet in another goroutine
+	packetSent := make(chan bool)
+	go sendPacket(t, packetSent)
 
-  timer := time.NewTimer(30 * time.Second)
+	timer := time.NewTimer(30 * time.Second)
 
-  for {
-    select {
-    case packet := <-packets:
-      t.Logf("captured packet: %+v", packet)
+	for {
+		select {
+		case packet := <-packets:
+			t.Logf("captured packet: %+v", packet)
 
-    case <-timer.C:
-      t.Logf("%v: no packets received, timing out", time.Now())
-      t.Fail()
+		case <-timer.C:
+			t.Logf("%v: no packets received, timing out", time.Now())
+			t.Fail()
 
-    case <-packetSent:
-      t.Logf("%v: packet sent", time.Now())
-      if testing.Short() {
-        timer.Reset(1 * time.Second)
-      }
+		case <-packetSent:
+			t.Logf("%v: packet sent", time.Now())
+			if testing.Short() {
+				timer.Reset(1 * time.Second)
+			}
 
-      continue
-    }
-    break
-  }
+			continue
+		}
+		break
+	}
 }
